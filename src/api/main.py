898180e4213f66d85b7d48bbb9f990c89d475a5d
dnnnnn0626 <77896070+dnnnnn0626@users.noreply.github.com>
2022-06-22 23:29:23 -0400

    encode function api needs adjustment

--- src/api/main.py
+++ src/api/main.py
@@ -1,5 +1,6 @@
 from typing import Tuple, Union
 from typing import List
+from matplotlib.font_manager import json_load
 import numpy as np
 from fastapi import FastAPI, HTTPException
 from PIL import Image
@@ -13,6 +14,7 @@ from io import StringIO
 import botocore
 from matplotlib import pyplot as plt
 import random
+import json
 
 app = FastAPI()
 
@@ -36,7 +38,8 @@ def rle_decode(mask_rle: str):
             img[lo:hi] = 1
     except:
         return {"Error Messages: ": "The input run-length string cannot be decode"}
-    return {"Image Pixel Array: ": img.reshape(shape)}
+    
+    return {"Image Pixel Array: ": str(img.reshape(shape))}
 
 @app.get("/image_number_of_ships/{num}")
 def image_num_ships(num: int):
@@ -123,7 +126,7 @@ def search_ship(t: str):
         img = Image.open(body)
         # create the numpy arrry of the image
         image_array = np.array(img.getdata()).reshape(img.size[0], img.size[1], 3)
-        return {"Image Pixel Array: ": image_array}
+        return {"Image Pixel Array: ": str(image_array)}
 
     # Error handling
     if t == "ship":
@@ -137,7 +140,7 @@ def search_ship(t: str):
 
 
 
-@app.get("/readImage_S/{ImageId}")
+@app.get("/readImage_S3/{ImageId}")
 def readImage_S3(ImageId: str):
         '''
         This function's purpose is to read the image from AWS S3 Bucket to the memory.
@@ -162,4 +165,115 @@ def readImage_S3(ImageId: str):
         img = Image.open(body)
         # create the numpy arrry of the image
         image_array = np.array(img.getdata()).reshape(img.size[0], img.size[1], 3)
-        return {"Image Pixel Array: ": image_array}
+        return {"Image Pixel Array: ": str(image_array)}
+
+
+
+@app.get("/run_length_encode/{ImageId}") # this one still need adjustment, probably is not imageid!!
+def rle_encode(ImageId: str):
+    '''
+    Input: ImageId: image file name
+    Return: run length encoding as string formated
+    '''
+
+    # AWS Credentials
+    aws_key_id = 'AKIA2ZQ35MMOGV7ZZ7PA'
+    aws_key = 'BrLIKkkVD+kdOQRz4TLp70K0YXZNaBHt6NVcfF2k'
+    bucket_name = 'airbus-detection-team-1-re'
+    object_key_img = 'assignment-1/train_v2/' + ImageId
+
+    # Error handling
+    try:
+        client = boto3.client('s3', aws_access_key_id = aws_key_id,
+            aws_secret_access_key = aws_key)
+    except:
+        return {"Error Messages: ": "No such key! Please enter a valid image name!" }
+    img_obj = client.get_object(Bucket = bucket_name, Key = object_key_img)
+    body = img_obj['Body']
+    img = Image.open(body)
+    # create the numpy arrry of the image
+    image_array = np.array(img.getdata()).reshape(img.size[0], img.size[1], 3)
+
+    pixels = image_array.flatten()
+    pixels = np.concatenate([[0], pixels, [0]])
+    runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
+    runs[1::2] -= runs[::2]
+    return {"String Formated RLE: ": str(' '.join(str(x) for x in runs))}
+
+
+
+@app.get("/image_and_masks/{ImageId}")
+def img_and_masks(ImageId: str):
+    ''' 
+    Input: ImageId : A string that contains the file name of the image in dataset; ImgShape was set to default as 768 by 768
+    Return: img: a numpy array represents the original image, all_masks: a numpy array represents the mask of the image
+    If the name of the iamge file is invalid, return "No such key! Please enter a valid image name!"
+    '''
+    ImgShape = [768, 768]
+    # AWS Credentials
+    aws_key_id = 'AKIA2ZQ35MMOGV7ZZ7PA'
+    aws_key = 'BrLIKkkVD+kdOQRz4TLp70K0YXZNaBHt6NVcfF2k'
+    bucket_name = 'airbus-detection-team-1-re'
+    object_key_img = 'assignment-1/train_v2/' + ImageId
+    object_key_csv = 'assignment-1/train_ship_segmentations_v2.csv'
+
+    # Error handling
+    
+    client = boto3.client('s3', aws_access_key_id = aws_key_id,
+            aws_secret_access_key = aws_key)
+    try:
+        img_obj = client.get_object(Bucket = bucket_name, Key = object_key_img)
+    except botocore.exceptions.ClientError:
+        return {"Error Messages: ":"No such key! Please enter a valid image name!"}
+    body = img_obj['Body']
+    img = Image.open(body)
+    # create the numpy array of the image
+    image_array = np.array(img.getdata()).reshape(img.size[0], img.size[1], 3)
+
+    # Read the csv file from S3
+    client = boto3.client('s3', aws_access_key_id = aws_key_id,
+            aws_secret_access_key = aws_key)
+    csv_obj = client.get_object(Bucket = bucket_name, Key = object_key_csv)
+    body = csv_obj['Body']
+    csv_string = body.read().decode('utf-8')
+    masks = pd.read_csv(StringIO(csv_string))
+    #num_masks = masks.shape[0]
+    #print('number of training images', num_masks)
+    img_masks = masks.loc[masks['ImageId'] == ImageId, 'EncodedPixels'].tolist()
+    # Take the individual ship masks and create a single mask array for all ships
+    all_masks = np.zeros(ImgShape)
+
+    # Reuse the decode function
+    def rle_decode(mask_rle: str):
+        '''
+        Input: mask_rle: run-length as string formated (start length)
+        shape: (height,width) of array to return 
+        Returns numpy array, 1 - mask, 0 - background
+        '''
+        shape = [768, 768]
+        s = mask_rle.split()
+        starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+        starts -= 1
+        ends = starts + lengths
+        img = np.zeros(shape[0] * shape[1], dtype=np.uint8)
+
+        for lo, hi in zip(starts, ends):
+            img[lo:hi] = 1
+        return img.reshape(shape)
+
+    for mask in img_masks:
+        # Note that NaN should compare as not equal to itself
+        if mask == mask:
+            all_masks += rle_decode(mask).T
+    
+    #fig, axarr = plt.subplots(1, 3, figsize=(15, 40))
+    #axarr[0].axis('off')
+    #axarr[1].axis('off')
+    #axarr[2].axis('off')
+    #axarr[0].imshow(img)
+    #axarr[1].imshow(all_masks)
+    #axarr[2].imshow(img)
+    #axarr[2].imshow(all_masks, alpha=0.4)
+    #plt.tight_layout(h_pad=0.1, w_pad=0.1)
+    #plt.show()
+    return {"Image Pixel Array:":str(image_array), "Image Mask Array:":str(all_masks)}
